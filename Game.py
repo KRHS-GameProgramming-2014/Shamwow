b586e54c9226a6e58fb4de27caf4fdee15f3d751
Maggamoose <zoe.parys@kearasrge.org>
2015-04-02 13:18:31 -0400

    Stuffs wow!

---
+++ Game.py
@@ -0,0 +1,3 @@
+import pygame, math, sys, os
+from Player import Player
+
---
+++ Player.py
@@ -0,0 +1,49 @@
+import pygame, math
+
+class Player():
+    def __init__(self, image, speed = [0,0], pos = [0,0]):
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.facing = "up"
+        self.speedx = speed[0]
+        self.speedy = speed[1]
+        self.speed = [self.speedx, self.speedy]
+        self.didBounceX = False
+        self.didBounceY = False
+        self.radius = (int(self.rect.height/2.0 + self.rect.width/2.0)/2) - 1
+        self.living = True
+        self.place(pos)
+        
+    def place(self, pos):
+        self.rect.center = pos
+        
+    def update(self, width, height):
+        self.didBounceX = False
+        self.didBounceY = False
+        self.move()
+        self.collideWall(width, height)
+        
+    def move(self):
+        self.speed = [self.speedx, self.speedy]
+        self.rect = self.rect.move(self.speed)
+        
+    def collideWall(self, width, height):
+        if not self.didBounceX:
+            if self.rect.left < 0 or self.rect.right > width:
+                self.speedx = -self.speedx
+                self.didBounceX = True
+        if not self.didBounceY:
+            if self.rect.top < 0 or self.rect.bottom > height:
+                self.speedy = -self.speedy
+                self.didBounceY = True
+        
+    def animate(self):
+        if self.waitCount < self.maxWait:
+            self.waitCount += 1
+        else:
+            self.waitCount = 0
+            self.changed = True
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
