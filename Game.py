f9e0d9d6babcffeada64d003b5d7d95edfa083ed
Ellie Mellott <eliana.mellott@kearsarge.org>
2015-05-12 13:18:12 -0400

    IT's WORKIGN ISH

--- Game.py
+++ Game.py
@@ -9,7 +9,7 @@ pygame.init()
  
 clock = pygame.time.Clock()
 
-width = 800 
+width = 1050
 height = 600
 size = width, height
 
@@ -67,7 +67,7 @@ while True:
     BackGround("RSC/Background Images/basichallway.png")
     player = Player([width/2, height/2])
 
-    level = Level(50, size)
+    level = Level(75, size)
     level.loadLevel("1")
     for b in blocks.sprites():
 		print b.rect.center
